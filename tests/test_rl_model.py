8789e759020a759e58d7db8d04a349d87770ddbd
ryan.bacastow <ryan.bacastow@tmsw.com>
2020-04-17 18:40:21 -0500

    changed test

--- tests/test_rl_model.py
+++ tests/test_rl_model.py
@@ -2,6 +2,6 @@ from app import rl_model
 
 
 def test_func():
-    result = rl_model.main()
+    result = rl_model.main(curr_pair="EUR/USD", period="1d", interval="1m", window_size=25, unit_side='left')
     assert result is not None
     assert type(result) is dict
